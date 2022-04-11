5f70833ebbd51a781c9829ac02abd842aad38fea
Dhaneswar-Mahali <103010427+Dhaneswar-Mahali@users.noreply.github.com>
2022-04-11 23:48:44 +0530

    Update admin.py

--- CBV_Project/Student_App/admin.py
+++ CBV_Project/Student_App/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from Student_App.models import Student
+#Create Student admin Class
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['id','sname','marks','email','mobile','address']
 
